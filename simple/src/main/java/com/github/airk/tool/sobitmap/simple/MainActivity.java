3f2d8d95becc3b1de864dee53f0a98a9d9fef5df
Kevin Liu <airk908@gmail.com>
2015-03-27 14:50:21 +0800

    Add Apache Liscence and copyright
    Signed-off-by: Kevin Liu <airk908@gmail.com>

--- simple/src/main/java/com/github/airk/tool/sobitmap/simple/MainActivity.java
+++ simple/src/main/java/com/github/airk/tool/sobitmap/simple/MainActivity.java
@@ -1,23 +1,34 @@
+/*
+ * Copyright 2015 Kevin Liu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.airk.tool.sobitmap.simple;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.github.airk.tool.sobitmap.Callback;
 import com.github.airk.tool.sobitmap.HuntException;
 import com.github.airk.tool.sobitmap.Options;
 import com.github.airk.tool.sobitmap.SoBitmap;
-
-import java.io.File;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -39,14 +50,13 @@ public class MainActivity extends ActionBarActivity {
                 .build();
         SoBitmap.getInstance(this).setDefaultOption(options);
         Uri uri = Uri.parse(BIG_SIZE_URL);
-//        Uri uri = Uri.fromFile(new File("/sdcard/DCIM/Camera/IMG_20150321_173922.jpg"));
         SoBitmap.getInstance(this).hunt(uri, new Callback() {
 
             @Override
             public void onHunted(Bitmap bitmap, BitmapFactory.Options options) {
                 progressBar.setVisibility(View.GONE);
                 imageView.setImageBitmap(bitmap);
-                textView.setText("sample:" + options.inSampleSize + " W:" + options.outWidth + " H:" + options.outHeight + " :" + bitmap.getAllocationByteCount() / 1000);
+                textView.setText("sample:" + options.inSampleSize + " W:" + options.outWidth + " H:" + options.outHeight);
             }
 
             @Override
